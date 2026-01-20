f81d68f0753776f0454ed44f9d23679d43fd8260
qwe2820435o2 <277096128@qq.com>
2026-01-21 11:38:52 +1300

    修正字段映射

--- prompt-generator-api/Models/DTOs/ReleaseWebhookRawRequestDto.cs
+++ prompt-generator-api/Models/DTOs/ReleaseWebhookRawRequestDto.cs
@@ -10,57 +10,57 @@ namespace tennis_wave_api.Models.DTOs;
 public class ReleaseWebhookRawRequestDto
 {
     /// <summary>
-    /// Email field (q17_email)
+    /// Email field (q43_email43)
     /// </summary>
-    [JsonPropertyName("q17_email")]
+    [JsonPropertyName("q43_email43")]
     public string? Email { get; set; }
 
     /// <summary>
-    /// Tag number field (q27_tagNo)
+    /// Tag number field (q44_tagNo44)
     /// </summary>
-    [JsonPropertyName("q27_tagNo")]
+    [JsonPropertyName("q44_tagNo44")]
     public string? TagNumber { get; set; }
 
     /// <summary>
-    /// Release date/time field (q20_date)
+    /// Release date/time field (q20_whatDay)
     /// </summary>
-    [JsonPropertyName("q20_date")]
+    [JsonPropertyName("q20_whatDay")]
     public JotFormDateDto? Date { get; set; }
 
     /// <summary>
-    /// Notes field (q22_notes)
+    /// Notes field (q22_notesYou)
     /// </summary>
-    [JsonPropertyName("q22_notes")]
+    [JsonPropertyName("q22_notesYou")]
     public string? Notes { get; set; }
 
     /// <summary>
-    /// Wind field (q32_wind)
+    /// Wind field (q32_estimateThe32)
     /// </summary>
-    [JsonPropertyName("q32_wind")]
+    [JsonPropertyName("q32_estimateThe32")]
     public string? Wind { get; set; }
 
     /// <summary>
-    /// Sex field (q33_sex)
+    /// Sex field (q33_malefemale)
     /// </summary>
-    [JsonPropertyName("q33_sex")]
+    [JsonPropertyName("q33_malefemale")]
     public string? Sex { get; set; }
 
     /// <summary>
-    /// Sun field (q34_sun)
+    /// Sun field (q34_wasIt34)
     /// </summary>
-    [JsonPropertyName("q34_sun")]
+    [JsonPropertyName("q34_wasIt34")]
     public string? Sun { get; set; }
 
     /// <summary>
-    /// Map Locator field (q35_mapLocator)
+    /// Map Locator field (q35_typeA35)
     /// </summary>
-    [JsonPropertyName("q35_mapLocator")]
+    [JsonPropertyName("q35_typeA35")]
     public string? MapLocator { get; set; }
 
     /// <summary>
-    /// GPS Location field (q36_gpsLocation)
+    /// GPS Location field (q36_typeA)
     /// </summary>
-    [JsonPropertyName("q36_gpsLocation")]
+    [JsonPropertyName("q36_typeA")]
     public string? GpsLocation { get; set; }
 
     /// <summary>
